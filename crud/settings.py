6546b94e48832922abf21e28a56d2cdfa0015e30
Anil <anilpujara125@gmail.com>
2021-10-26 10:01:00 +0545

    commit

--- crud/settings.py
+++ crud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '42pb5*i!_xb+&9*%2%v5qlh^h9dumyjl=n5j3uit521aq-mjyw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','crudoperaion.herokuapp.com/']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- crud/urls.py
+++ crud/urls.py
@@ -1,12 +1,14 @@
 
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
 from mycrud.views import delete, home, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
     path('delete/<int:id>/',delete,name='delete'),
-    path('update/<int:id>/',update,name='update')
+    path('update/<int:id>/',update,name='update'),
+    
 ]
