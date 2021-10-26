c18481aef53fe228a018ab710c52003e0d9869b8
Anil <pujarapujara125@gmail.com>
2021-10-26 10:55:51 +0545

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1','crudoperaion.herokuapp.com/']
 
 
-# Application definition
+# Application definitiongit
 
 INSTALLED_APPS = [
     'django.contrib.admin',
